1b6e6df43ec3184239e4c2a8cf8530dd4c9fe15d
borellaster <borellaster@gmail.com>
2015-11-19 21:09:27 -0200

    atualizacoes diversas

--- ui.R
+++ ui.R
@@ -11,10 +11,12 @@ shinyUI(pageWithSidebar(
     tabsetPanel(
       # Show a plot of the generated distribution
       tabPanel("Gráfico", plotOutput("distPlot")),
-      tabPanel("Resultados", dataTableOutput(outputId="tableResultsPlant")),
-      tabPanel("Balanço Hídrico", dataTableOutput(outputId="tableFinal")),
+      tabPanel("Resultados", mainPanel(tabsetPanel (
+        tabPanel("Simulação do Crescimento", dataTableOutput(outputId="tableResultsPlant")),
+        tabPanel("Balanço Hídrico do Solo", dataTableOutput(outputId="tableResultsSoil")),
+        tabPanel("Balanço Hídrico Sazonal", dataTableOutput(outputId="tableFinal"))
+      ))),
       tabPanel("Estações", dataTableOutput(outputId="tableResultsStation")),
-      tabPanel("Dados por dia", dataTableOutput(outputId="tableResultsSoil")),
       tabPanel("Sobre", includeMarkdown("Documentacao/index.Rmd"))
     )
   )
